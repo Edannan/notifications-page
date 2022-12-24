62d0248e04dd8a76a0d51227322ffab04bdff6d5
Emmanuel Danso Annan <emmanueldansoannan@gmail.com>
2022-12-24 18:24:24 +0000

    First update

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".mark a").click(function(){
+        $(".pm").css({"background-color":"white", "border": "1px solid darkgray"})
+        $(".mark a").css("color","darkgray")
+
+    })
+})
+
+
+
